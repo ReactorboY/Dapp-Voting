857645097b41ab57e56286e3218847e309b1f4e3
Mohd Hussain <samsteve124@gmail.com>
2019-08-24 13:06:14 +0530

    Smoke Test

---
+++ contracts/election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+    // store candidate
+    string public candidate;
+    // read candidate
+    // constructor
+    constructor () public {
+        candidate = "Candidate 1";
+    }
+}
